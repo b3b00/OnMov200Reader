d92939b3a00591f44597ec58e5100d81fd4af9c3
Olivier Duhart <olivier.duhart@sage.com>
2020-07-10 17:42:51 +0200

    command line tool

--- Program/Program.cs
+++ Program/Program.cs
@@ -12,54 +12,66 @@ namespace porgram
         
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("______________________________");
-            Console.WriteLine("___ USER                ___");
-            Console.WriteLine("______________________________");
-            
-            Stream stream = File.Open(@"C:\Users\olduh\Desktop\perso\onmov200\USER.BIN",FileMode.Open);
-            var user = OnMov200Schemas.USER.Read(stream);
-            foreach (var r in user)
+            if (args.Length < 1)
             {
-                Console.WriteLine($"{r.Key} : {r.Value}");
+                Environment.Exit(1);
             }
-            stream.Close();
             
+            
+
+            string root = args[0];
+
+            var files = Directory.GetFiles(root, "*.OMD");
+            ;
+            
+
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                string activity = info.Name.Replace(".OMD", "");
+                ExtractActivity(root, activity);
+            }
+            
+            
+            
+
+            ;
+        }
+
+        private static void ExtractActivity(string root, string activity)
+        {
             Console.WriteLine("______________________________");
             Console.WriteLine("___ OMH                    ___");
             Console.WriteLine("______________________________");
-            
-            stream = File.Open(@"C:\Users\olduh\Desktop\perso\onmov200\DATA\ACT_0003.OMH",FileMode.Open);
+
+            var stream = File.Open($@"{root}\{activity}.OMH", FileMode.Open);
             var omh = OnMov200Schemas.OMH.Read(stream);
             foreach (var r in omh)
             {
                 Console.WriteLine($"{r.Key} : {r.Value}");
             }
 
-            DateTime startTime = new DateTime(2000+(int) omh["year"], (int) omh["month"], (int) omh["day"],
-                (int) omh["hour"], (int) omh["min"],0);
-            
+            DateTime startTime = new DateTime(2000 + (int) omh["year"], (int) omh["month"], (int) omh["day"],
+                (int) omh["hour"], (int) omh["min"], 0);
+
             Console.WriteLine("______________________________");
             Console.WriteLine("___ OMD                    ___");
             Console.WriteLine("______________________________");
-            
-            stream = File.Open(@"C:\Users\olduh\Desktop\perso\onmov200\DATA\ACT_0003.OMD",FileMode.Open);
+
+            stream = File.Open($@"{root}\{activity}.OMD", FileMode.Open);
             OMDParser parser = new OMDParser();
             try
             {
-                
                 var datas = parser.Parse(stream, startTime);
                 if (datas != null && datas.Any())
                 {
-                    GpxSerializer.Serialize(datas,"./gpx.gpx");
+                    GpxSerializer.Serialize(datas, $"./{activity}.gpx");
                 }
             }
             catch (Exception e)
             {
                 ;
             }
-
-            ;
         }
     }
 }

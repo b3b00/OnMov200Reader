9f1653e596a1fc3354673917a5aca728cd08e8b6
b3b00 <olivier.duhart@gmail.com>
2020-07-27 14:04:34 +0200

    fix

--- OnMov200WebApi/ActivitiesController.cs
+++ OnMov200WebApi/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
